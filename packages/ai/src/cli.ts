095996b38a7b48c33eefd87115201d293ed0f3c5
jurgob <jurgob@users.noreply.github.com>
2024-11-12 00:10:24 +0000

    text to image

--- packages/ai/src/cli.ts
+++ packages/ai/src/cli.ts
@@ -1,11 +1,25 @@
-import { questionAnswering } from "./index.ts";
-
+import { createHfClient } from "./index.ts";
+import crypto from "node:crypto";
+import os from "node:os";
+import fs from "node:fs";
 const model = "facebook/blenderbot-400M-distill";
-    const inputs = "What is the captital of France?";
-    const context = "The capital of France is Budoia.";
+const inputs = "What is the captital of France?";
+const context = "The capital of France is Budoia.";
 
-questionAnswering({
-    question: inputs,
-    context: context,
-    model: model
-}).then(console.log);
+const {questionAnswering,textToImage} = await createHfClient();
+
+// questionAnswering({
+//     question: inputs,
+//     context: context,
+//     model: model
+// }).then(console.log).catch(console.error);;
+
+textToImage({
+    text: "a picture of a green bird"
+}).then(async (res) => {
+    const fileName = os.tmpdir() + `/downloaded_image_${crypto.randomUUID()}.jpg`;
+    fs.writeFileSync(fileName, Buffer.from(await res.arrayBuffer()));
+    console.log(`image saved to ${fileName}`);    
+}).catch(console.error);
+
+console.log();
--- packages/ai/src/index.ts
+++ packages/ai/src/index.ts
@@ -1,48 +1,32 @@
 import { HfInference } from "@huggingface/inference";
+
 const TOKEN = process.env.HF_TOKEN;
 
-const hf = new HfInference(TOKEN);
+export async function createHfClient(){
+  const hf = new HfInference(TOKEN);
 
-export async function questionAnswering({question, context, model}: {question: string, context: string, model: string}) {
-    // const res = await hf.questionAnswer({
-    //     model: 'deepset/roberta-base-squad2',
-    //     inputs: {
-    //       question,
-    //       context
-    //     }
-    //   })
-    // console.log('hf',hf);
-    // return await hf.chatCompletion({
-    //     model: "google/gemma-2-2b-it",
-    //     messages: [{ role: "user", content: "Hello, nice to meet you!" }],
-    //     max_tokens: 512,
-    //     temperature: 0.1,
-    //   });
-    
-      // let out = "";
-      // for await (const chunk of hf.chatCompletionStream({
-      //   model: "google/gemma-2-2b-it",
-      //       messages: [{ role: "user", content: "Hello, nice to meet you!" }],
-      //       max_tokens: 512,
-      //       temperature: 0.1,
-      // })) {
-      //   if (chunk.choices && chunk.choices.length > 0) {
-      //     out += chunk.choices[0].delta.content;
-      //     process.stdout.write(out);
-      //   }
-      // }
-      // return out
-    
-      const result = await hf.textGeneration({
-        model,
-        inputs: `${context}  ${question}`,
-        parameters: {
-          max_new_tokens: 50,  // Max number of new tokens to generate
-          temperature: 0.1,    // Sampling temperature for text diversity
-        },
-      });
-      return result;
+  const questionAnswering = async ({question, context, model}: {question: string, context: string, model: string}) => {
+    const result = await hf.textGeneration({
+      model,
+      inputs: `${context}  ${question}`,
+      parameters: {
+        max_new_tokens: 50,  // Max number of new tokens to generate
+        temperature: 0.1,    // Sampling temperature for text diversity
+      },
+    });
+    return result;
+  }
 
+  const textToImage = async ({text, model="black-forest-labs/FLUX.1-dev"}: {text: string, model?: string}) => {
+    return await hf.textToImage({
+      model,
+      inputs: text
+    });
+  }
+
+
+  return {
+    questionAnswering,
+    textToImage
+  }
 }
-
-// questionAnswering('What is the capital of France?', 'The capital of France is Paris.').then(console.log);
